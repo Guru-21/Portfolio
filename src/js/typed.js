b136662e397f094e8a83133b3bad3c14de391082
Gurpreet Singh <68956821+Guru-21@users.noreply.github.com>
2025-01-25 22:44:58 +0530

    Update typed.js
    Changes in Typed

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 var options = {
-  strings: ["Nice to meet you!", "I'm a Java-FullStack Developer", 'I have 1+ years of experience in IT Industry.'],
+  strings: ["Nice to meet you!", "I'm a Java Backend Developer", 'I have 3+ years of experience in the IT Industry.'],
   typeSpeed: 120,
   backSpeed: 50,
   backDelay: 1500,
